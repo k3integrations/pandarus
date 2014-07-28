958d4d2c36a03388d96ffb056ed2abdae176202c
dallas <code@dallasreedy.com>
2014-07-28 11:47:29 -0600

    was missing a few attributes for Account

--- lib/pandarus/models/account.rb
+++ lib/pandarus/models/account.rb
@@ -5,7 +5,7 @@ module Pandarus
     attr_accessor :id, :name, :parent_account_id, :root_account_id,
       :workflow_state, :default_storage_quota_mb,
       :default_user_storage_quota_mb, :default_group_storage_quota_mb,
-      :default_time_zone
+      :default_time_zone, :sis_account_id, :integration_id, :sis_import_id
 
     def self.attribute_map
       { id: { external: 'id', container: false, type: 'Integer' },
@@ -16,7 +16,10 @@ module Pandarus
         default_storage_quota_mb: { external: 'default_storage_quota_mb', container: false, type: 'Integer' },
         default_user_storage_quota_mb: { external: 'default_user_storage_quota_mb', container: false, type: 'Integer' },
         default_group_storage_quota_mb: { external: 'default_group_storage_quota_mb', container: false, type: 'Integer' },
-        default_time_zone: { external: 'default_time_zone', container: false, type: 'String' }
+        default_time_zone: { external: 'default_time_zone', container: false, type: 'String' },
+        sis_account_id: { external: 'sis_account_id', container: false, type: 'String' },
+        integration_id: { external: 'integration_id', container: false, type: 'String' },
+        sis_import_id: { external: 'sis_import_id', container: false, type: 'Integer' }
       }
     end
   end

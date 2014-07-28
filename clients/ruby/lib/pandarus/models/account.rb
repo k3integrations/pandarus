ff77edd959dd6857726dc7e116e80e9100bd5e95
dallas <code@dallasreedy.com>
2014-07-28 09:56:19 -0600

    added in the Account model
    This model is missing due to an issue with the YARD code that generates the swagger JSON files where it gets tripped up by classes that have been re-opened in Ruby.
    See https://github.com/instructure/pandarus/issues/3 for a bit more on that.

---
+++ clients/ruby/lib/pandarus/models/account.rb
@@ -0,0 +1,23 @@
+require 'pandarus/model_base'
+
+module Pandarus
+  class Account < ModelBase
+    attr_accessor :id, :name, :parent_account_id, :root_account_id,
+      :workflow_state, :default_storage_quota_mb,
+      :default_user_storage_quota_mb, :default_group_storage_quota_mb,
+      :default_time_zone
+
+    def self.attribute_map
+      { id: { external: 'id', container: false, type: 'Integer' },
+        name: { external: 'name', container: false, type: 'String' },
+        parent_account_id: { external: 'parent_account_id', container: false, type: 'Integer' },
+        root_account_id: { external: 'root_account_id', container: false, type: 'Integer' },
+        workflow_state: { external: 'workflow_state', container: false, type: 'String' },
+        default_storage_quota_mb: { external: 'default_storage_quota_mb', container: false, type: 'Integer' },
+        default_user_storage_quota_mb: { external: 'default_user_storage_quota_mb', container: false, type: 'Integer' },
+        default_group_storage_quota_mb: { external: 'default_group_storage_quota_mb', container: false, type: 'Integer' },
+        default_time_zone: { external: 'default_time_zone', container: false, type: 'String' }
+      }
+    end
+  end
+end
